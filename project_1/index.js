88fc19db2781e171cf13ab7f7f6a8233c72f0e1b
tuandoan <doan.manhtuan511@gmail.com>
2019-11-07 00:13:00 +0700

    add mqtt

--- project_1/index.js
+++ project_1/index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import map from "./map";
+import map from './map';
+import test from './test';
 
-AppRegistry.registerComponent(appName, () => map);
+AppRegistry.registerComponent(appName, () => test);
---
+++ project_1/test.js
@@ -0,0 +1,64 @@
+import {Client, Message} from 'react-native-paho-mqtt';
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+//Set up an in-memory alternative to global localStorage
+const myStorage = {
+  setItem: (key, item) => {
+    myStorage[key] = item;
+  },
+  getItem: key => myStorage[key],
+  removeItem: key => {
+    delete myStorage[key];
+  },
+};
+
+// Create a client instance
+const client = new Client({
+  uri: 'wss://soldier.cloudmqtt.com:30718/wss',
+  clientId: 'clientId',
+  storage: myStorage,
+});
+
+// set event handlers
+client.on('connectionLost', responseObject => {
+  if (responseObject.errorCode !== 0) {
+    console.log(responseObject.errorMessage);
+  }
+});
+client.on('messageReceived', message => {
+  console.log(message.payloadString);
+});
+
+// connect the client
+
+var options = {
+  userName: 'zpmrungb',
+  password: 'RLWW4l_DSuYI',
+};
+
+client
+  .connect(options)
+  .then(() => {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log('onConnect');
+    console.log(client.subscribe('test'));
+  })
+  //.then(() => {
+  //const message = new Message('Hello');
+  //message.destinationName = 'World';
+  //client.send(message);
+  //})
+  .catch(responseObject => {
+    if (responseObject.errorCode !== 0) {
+      console.log('onConnectionLost:' + responseObject.errorMessage);
+    }
+  });
+
+export default function() {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  );
+}
